ff1b5c1bdebb365cf86d604aab0e31dea708ad18
Jhades777 <jhadeswinderencarnacion@gmail.com>
2026-02-27 18:08:13 -0400

    Animacion del salto y giros

--- Assets/codigo/MovimientoJugador.cs
+++ Assets/codigo/MovimientoJugador.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public Animator animator;
+    public float velocidad = 5f;
     public float jumpForce = 6f; // Fuerza del salto
     public float rayLength = 0.6f; // Longitud del rayo para detectar el suelo
-    public LayerMask groundLayer; // Capa del suelo para detecci n
+    public LayerMask groundLayer; // Capa del suelo para deteccion
 
     private Rigidbody2D rb;
     private bool isGrounded;
@@ -16,18 +17,37 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
+    
+    
 
     void Update()
+    //Velocidad del movimiento horizontal, multiplicada por el
+    //tiempo para que sea consistente en diferentes velocidades de fotogramas    
     {
         float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+    rb.linearVelocity = new Vector2(moveInput * velocidad, rb.linearVelocity.y);
+
+    // Animación
+    animator.SetFloat("movement", Mathf.Abs(moveInput));
+
+    // Giro 
+    if (moveInput > 0) transform.localScale = new Vector3(6.0518f, 5.6918f, 1f);
+    else if (moveInput < 0) transform.localScale = new Vector3(-6.0518f, 5.6918f, 1f);
+
+        //Salto del personaje, se activa al presionar la barra espaciadora
+        //y solo si el personaje esta en el suelo
+       
+        rb.linearVelocity = new Vector2(moveInput * velocidad, rb.linearVelocity.y);
 
         isGrounded = IsGrounded();
+
+        animator.SetBool("isGrounded", isGrounded);
 
         if (isGrounded && Input.GetButtonDown("Jump"))
             Jump();
     }
-
+    
     void Jump()
     {
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
